0b3dfd0fc917ad75c5586c7007e959ebb96ae0b4
hiann <hiannmanso@gmail.com>
2023-06-03 21:49:21 -0300

    feat: database task model

---
+++ prisma/migrations/20230604004714_task_add/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('ALTA', 'MEDIA', 'BAIXA');
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "describe" TEXT NOT NULL,
+    "finalDate" TIMESTAMP(3) NOT NULL,
+    "priority" "Priority" NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import router from './routers/router'
+import {handdleError} from './middlewares/handdleError'
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+server.use(router)
+server.use(handdleError)
+// ,
+export default server
